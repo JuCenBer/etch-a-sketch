3a9b3b91798ac8cbedab310d8fe76f549cfd129b
JuCenBer <El_Jbc_99@hotmail.com>
2021-08-18 20:31:36 -0300

    Added a input to change the amount of pixels in the board. Added a button to clean the board.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const board = document.querySelector("#board");
+let inputVal;
 
-function boardGrid(){
+function boardGrid(limit){
     let row,col;
-    for(row=1; row<=16; row++){
-        console.log("row= " + row);
-        for(col=1; col<=16; col++){
-            console.log("col= " + col);    
-            let item = document.createElement("div");
-            board.appendChild(item);
-            item.style.gridArea= `${row}/${col}`;
+    for(row=1; row<=limit; row++){
+        for(col=1; col<=limit; col++){   
+            const pixel = document.createElement("div");
+            pixel.style.backgroundColor="white";
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mouseenter',()=>{
+                pixel.style.backgroundColor= "darkGrey";
+            })
+            board.appendChild(pixel);
+            pixel.style.gridArea= `${row}/${col}`;
         }
     }
 }
 
-boardGrid();
+function clearBoard(){//hay que hacer este clear
+    let pixels= document.querySelector("#board").childNodes;
+    for (let i=0; i<pixels.length; i++){
+        pixels[i].style.backgroundColor="white";
+    }
+}
+
+function getInputValue(){
+    inputVal = document.getElementById("subText").value;
+    document.getElementById('board').innerHTML = ''; //limpia el innerHTML del board
+    boardGrid(inputVal);
+}
