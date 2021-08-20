a8d304ab67893515dbff2f3dacde4450745abfca
JuCenBer <El_Jbc_99@hotmail.com>
2021-08-19 23:11:21 -0300

    Modified the button control's code. Fixed a bug in which, once you press the darken button, you cannot turn it back to the 'normal' mode. Added a Normal button mode

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const board = document.querySelector("#board");
+const buttons = document.querySelectorAll("#buttons");
+const normal = document.querySelector("#normal");
+const clear = document.querySelector("#clear");
+const darken = document.querySelector("#darken");
+const createGrid = document.querySelector("#createGrid");
+let buttonMode= "normal";
 let inputVal;
 
 function boardGrid(limit){
@@ -17,7 +23,16 @@ function boardGrid(limit){
     }
 }
 
-function clearBoard(){//hay que hacer este clear
+function normalMode(){
+    let pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel =>{
+        pixel.addEventListener('mouseenter',()=>{
+            pixel.style.backgroundColor="darkgrey";
+        })
+    })
+}
+
+function clearBoard(){ //simplemente pinta de blanco los child divs
     let pixels= document.querySelector("#board").childNodes;
     for (let i=0; i<pixels.length; i++){
         pixels[i].style.backgroundColor="white";
@@ -29,3 +44,48 @@ function getInputValue(){
     document.getElementById('board').innerHTML = ''; //limpia el innerHTML del board
     boardGrid(inputVal);
 }
+
+function darkenMode(){
+    let pixels= document.querySelectorAll(".pixel");
+    pixels.forEach(pixel =>{
+        pixel.addEventListener('mouseenter',()=>{
+            pixel.style.backgroundColor="black";
+        })
+    })
+}
+
+boardGrid(30);//por default se inicializa con un grid de 30x30
+
+
+normal.addEventListener('click', () => {
+    buttonMode = "normal";
+    normalMode();
+})
+
+clear.addEventListener('click', () => {
+    if(buttonMode === "clear"){
+        buttonMode="normal";
+        normalMode();
+    }
+    else{
+        buttonMode= "clear";
+        clearBoard();
+    }
+    
+})
+
+darken.addEventListener('click', () => {
+    if(buttonMode === "darken"){
+        buttonMode="normal";
+        normalMode();
+    }
+    else{
+        buttonMode = "darken";
+        darkenMode();
+    }
+     
+})
+
+createGrid.addEventListener('click', () => {
+    getInputValue();
+})
